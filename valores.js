1e34dafbeb8cb03a8f7e14b6b99ef9f12eeb576a
Edmoun <edmounramirez@gmail.com>
2021-12-28 15:10:01 -0400

    primer commit

---
+++ valores.js
@@ -0,0 +1,12 @@
+40 ("tipo de dato number")
+
+"Esto es un string"
+
+true, false ("tipo de dato boolean")
+
+null, undefined ("tipo de datos vacios")
+
+[1,2,3] ("tipo de dato arreglos") // ("pero un arreglo es de tipo objeto");
+
+
+{precio:"56"} ("tipo de dato orientado a objeto");
---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Edmoun";
+
+var edad;
+// declarando
+
+var edad = 17;
+// inicializando
+
+
+var frutas = ["Manzana","uva"];
+
+var persona = {
+    nombre = "Edmoun",
+    edad = "17"
+}
+
+hola MutationRecor
